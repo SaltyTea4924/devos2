5c8e31cd9299ad27e53b79ec472e1d3921392530
SaltyTea4924 <111988607+SaltyTea4924@users.noreply.github.com>
2024-10-31 15:50:17 +0000

    initial commit

---
+++ test/test.js
@@ -0,0 +1,28 @@
+var main = require('../index');
+var assert = require('assert');
+describe('Regular fibonacci value', function() {
+    it('should return 5 for Fib(5)', function() {
+        assert.strictEqual(main.fibonacci(5), 5);
+      });
+});
+
+describe('First two terms', function() {
+  it('should return 0 and 1 for first two terms', function() {
+      assert.strictEqual(main.fibonacci(0), 0);
+      assert.strictEqual(main.fibonacci(1), 1);
+    });
+});
+
+describe('Regula factorial value', function() {
+    it('should return 120 for Fac(5)', function() {
+        assert.strictEqual(main.factorial(5), 120);
+      });
+});
+
+describe('First two terms', function() {
+    it('should return 1 for both 0 and 1', function() {
+        assert.strictEqual(main.factorial(0), 1);
+        assert.strictEqual(main.factorial(1), 1);
+      });
+  });
+  
